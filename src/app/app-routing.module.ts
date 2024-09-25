8b405a1598b958d913225fc07f34dbb707e3a708
jibe <jibeharmoniq@gmail.com>
2024-09-25 16:45:17 +0200

    Feat:Guard implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './auth/authentication/authentication.component';
+import { CategorieComponent } from './categorie/categorie.component';
+import { authenticationGuard } from './auth/authentication/authentication.guard';
 
 const routes: Routes = [
-  { path: 'signup', component: AuthenticationComponent },
+  { path: 'auth', component: AuthenticationComponent },
+  { path: '', component: CategorieComponent, canActivate:[authenticationGuard] },
 ];
 
 @NgModule({
