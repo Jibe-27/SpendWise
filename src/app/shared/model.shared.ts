440abf00cfb1a3d7097d56dade0f608b696c82df
Farid <farid.ousmane01@gmail.com~>
2024-09-28 01:24:41 +0200

    Feat: add  interface Expense && Category + Update user for the budget

--- src/app/shared/model.shared.ts
+++ src/app/shared/model.shared.ts
@@ -1,9 +1,24 @@
-export interface Language{
-    name: string;
-    code: string;
+export interface Language {
+  name: string;
+  code: string;
 }
-export interface User{
-  email:string;
-  password:string;
-  name:string;
+export interface User {
+  email: string;
+  password: string;
+  name: string;
+  budget: number;
+}
+
+export interface Expense {
+  datetime: string;
+  category: Category;
+  store: string;
+  amount: number;
+  description?: string;
+}
+
+export interface Category {
+  name: string;
+  color: string;
+  icon: string;
 }
