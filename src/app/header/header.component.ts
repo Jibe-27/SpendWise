07e212dc587f189e040953f042b514b54655c6d0
Farid <farid.ousmane01@gmail.com~>
2024-09-29 23:51:37 +0200

    add user email in the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../authentication/authentication.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  userEmail: string | null = null;
+
   constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    const currentUser = this.authService.getUser();
+    if (currentUser && currentUser.id) {
+      this.authService
+        .getUserById(currentUser.id.toString())
+        .subscribe((user) => {
+          this.userEmail = user.email;
+        });
+    }
+  }
+
   logout() {
     this.authService.logout();
     this.router.navigate(['/auth']);
